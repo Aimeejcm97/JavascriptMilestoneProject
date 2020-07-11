23528843be480aae052a6ab3bbc135a520d57860
Aimeejcm97 <56825183+Aimeejcm97@users.noreply.github.com>
2020-07-11 15:59:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,30 @@
 const solve = document.querySelector('.solvebt');
 solve.addEventListener('click', calculate);
 function calculate() {
+  let number1 = parseFloat(ip_1.value);
+  let number2 = parseFloat(ip_2.value);
+  let answer;
   const ip_1 = document.querySelector('.ip_1');
   const ip_2 = document.querySelector('.ip_2');
   const answer_3 = document.querySelector('.answer_3');
   const operation = document.querySelector('#operation');
-  let number1 = parseFloat(ip_1.value);
-  let number2 = parseFloat(ip_2.value);
-  let answer;
-
-
   
   switch (operation.value) {
-
     case "subtract":
       answer = number1 - number2;
       answer_3.innerHTML = answer;
       break;
-
     case "add":
       answer = number1 + number2;
       answer_3.innerHTML = answer;
       break;
-
     case "divide":
       answer = number1 / number2;
       answer_3.innerHTML = answer;
-      break;
-
+      break
     case "multiply":
       answer = number1 * number2;
       answer_3.innerHTML = answer;
       break;
-
-
   }
 }
